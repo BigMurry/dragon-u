2813309c95709fe9998e4d983ccda3784a144db7
BigMurry <hhdiao@siom.ac.cn>
2018-12-26 15:18:27 +0800

    fix build

--- pm2.config.js
+++ pm2.config.js
@@ -2,8 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'dragon:ui',
-      script: './ui.server.js',
+      script: 'ui.server.js',
+      max_memory_restart: '150M',
       env: {
+        PORT: 8001,
         NODE_ENV: 'production'
       }
     }
